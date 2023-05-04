950f7c87bf3a43adb330c94777efa71b15c5ed84
Jahidhasan3323 <30716686+Jahidhasan3323@users.noreply.github.com>
2023-05-04 16:06:42 +0600

    remove all route

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,11 +2,3 @@
 
 use App\Http\Controllers\Api\AutodeskForge;
 use Illuminate\Support\Facades\Route;
-
-Route::get('test', function (){
-   dd('hello');
-});
-//Route::get('get-token', [AutodeskForge::class, 'getToken'])->middleware('api');
-//
-//Route::get('get-bucket', [AutodeskForge::class, 'getBucket']);
-//Route::get('delete-bucket', [AutodeskForge::class, 'deleteBucket']);
